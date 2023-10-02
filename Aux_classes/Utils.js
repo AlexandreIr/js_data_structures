c7dec3e0a52d9ba24747246c100e7a61dfe8f754
Alexandre Fernandes <alex.silva250@hotmail.com>
2023-10-01 22:29:18 -0300

    some funcionalities

--- Aux_classes/Utils.js
+++ Aux_classes/Utils.js
@@ -24,7 +24,7 @@ export function defaultEquals(a,b){
     return a===b;
 }
 
-const Compare={
+export const Compare={
     LESS_THAN:-1,
     BIGGER_THAN:1
 }
--- Trees/BinarySearchTree.js
+++ Trees/BinarySearchTree.js
@@ -1,8 +1,31 @@
-import { defaultCompare } from "../Aux_classes/Utils";
+import { Compare, TreeNode, defaultCompare } from "../Aux_classes/Utils";
 
 export default class BinarySearchTree{
     constructor(compareFn=defaultCompare){
         this.compareFn=compareFn;
         this.root=null
     }
+
+    insert(key){
+        if(this.root==null) this.root= new TreeNode(key);
+        else this.insertNode(this.root, key);
+    }
+
+    insertNode(node, key){
+        if(this.compareFn(key, node.key)===Compare.LESS_THAN){
+            if(node.left==null){
+                node.left = new TreeNode(key);
+            }else{
+                this.insertNode(node.left, key);
+            }
+        }else{
+            if(node.right==null){
+                node.right=new TreeNode(key);
+            }else{
+                this.insertNode(node.right, key);
+            }
+        }
+    }
+
+    
 }
